c21f5f9ba7cbb2058638c16edadeaa1d72ad47aa
nishanarola <nishanarola25@gmail.com>
2026-07-27 17:56:08 +0530

    Add small U logo watermark

--- src/pages/AuthLayout.jsx
+++ src/pages/AuthLayout.jsx
@@ -73,8 +73,8 @@ export default function AuthLayout({ eyebrow, title, subtitle, children }) {
             component="svg"
             viewBox="0 0 72 72"
             sx={{
-              width: 30,
-              height: 30,
+              width: 56,
+              height: 56,
               mb: -1
             }}
           >
